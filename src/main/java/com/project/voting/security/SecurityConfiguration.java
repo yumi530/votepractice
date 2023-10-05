c73ba7579cfe2be68a4a318af081279e778807d7
yumin lee <chenj7498@gmail.com>
2023-10-05 16:04:05 +0900

    관리자, 사용자 화면 분리

--- src/main/java/com/project/voting/security/SecurityConfiguration.java
+++ src/main/java/com/project/voting/security/SecurityConfiguration.java
@@ -38,7 +38,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     http
       .formLogin()
       .loginPage("/admin/login")
-//                .successHandler(successHandler())
       .failureHandler(getFailureHandler())
       .usernameParameter("username")
       .passwordParameter("password")
@@ -46,7 +45,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
       .and()
       .logout()
       .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-      .logoutSuccessUrl("/")
+      .logoutSuccessUrl("/admin/election/electionList")
       .invalidateHttpSession(true)
       .and()
       .exceptionHandling()
@@ -55,8 +54,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
       .httpBasic().disable()
       .csrf().disable()
       .authorizeRequests()
-//                .antMatchers("/")
-//                .permitAll();
       .antMatchers("/admin/**")
       .hasAuthority("ADMIN");
   }
