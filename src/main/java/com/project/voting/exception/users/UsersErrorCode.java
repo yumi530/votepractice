8f075585657237d9d284d545b5109b3044a821e4
yumin lee <chenj7498@gmail.com>
2023-10-18 17:06:38 +0900

    에러페이지 및 익셉션 수정

--- src/main/java/com/project/voting/exception/users/UsersErrorCode.java
+++ src/main/java/com/project/voting/exception/users/UsersErrorCode.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum UsersErrorCode {
 
   USERS_LIST_NOT_FOUND("선거인 명부에 존재하지 않습니다."),
-  NOT_LOGGED_IN("로그인을 해주십시오.");
+  NOT_LOGGED_IN("로그인을 해주십시오."),
+  USERS_FILE_NOT_DELETED("명부 삭제가 불가 합니다.");
 
   private final String description;
 
