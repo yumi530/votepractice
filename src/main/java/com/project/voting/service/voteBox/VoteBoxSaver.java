8b23fd996f2c4be3904a1ba33532da1f90bc307e
yumin lee <chenj7498@gmail.com>
2023-10-23 19:41:20 +0900

    VoteboxService 수정

--- src/main/java/com/project/voting/service/voteBox/VoteBoxSaver.java
+++ src/main/java/com/project/voting/service/voteBox/VoteBoxSaver.java
@@ -1,25 +1,17 @@
-package com.project.voting.service.new_votebox;
+package com.project.voting.service.voteBox;
 
-import com.project.voting.domain.candidate.Candidate;
-import com.project.voting.domain.candidate.CandidateRepository;
 import com.project.voting.domain.voteBox.VoteBox;
 import com.project.voting.domain.voteBox.VoteBoxRepository;
 import com.project.voting.dto.voteBox.VoteBoxDto;
-import com.project.voting.exception.vote_box.VoteBoxCustomException;
-import com.project.voting.exception.vote_box.VoteBoxErrorCode;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-
-@Service
+@Component
 @RequiredArgsConstructor
 public class VoteBoxSaver {
-
   private final VoteBoxRepository voteBoxRepository;
-  private final CandidateRepository candidateRepository;
 
   public VoteBox saveProsCons(VoteBoxDto voteBoxDto) {
     VoteBox voteBox = createVoteBox(voteBoxDto);
@@ -56,20 +48,5 @@ public class VoteBoxSaver {
     voteBox.setVoteId(voteBoxDto.getVoteId());
     voteBox.setUsersPhone(voteBoxDto.getUsersPhone());
     return voteBox;
-  }
-
-  public boolean isValid(VoteBoxDto voteBoxDto) {
-
-    Optional<VoteBox> optionalVoteBox = voteBoxRepository.findByCandidateIdAndUsersPhone(
-      voteBoxDto.getCandidateId(), voteBoxDto.getUsersPhone());
-    if (optionalVoteBox.isPresent()) {
-      throw new VoteBoxCustomException(VoteBoxErrorCode.VOTE_DUPLICATED);
-    }
-    List<Candidate> candidateList = candidateRepository.findAllCandidateIdByVoteId(
-      voteBoxDto.getVoteId());
-    if (candidateList == null) {
-      throw new VoteBoxCustomException(VoteBoxErrorCode.VOTE_NOT_FOUND);
-    }
-    return true;
   }
 }
--- src/main/java/com/project/voting/service/voteBox/VoteBoxService.java
+++ src/main/java/com/project/voting/service/voteBox/VoteBoxService.java
@@ -1,13 +1,14 @@
-//package com.project.voting.service.voteBox;
-//
-//import com.project.voting.domain.voteBox.VoteBox;
-//import com.project.voting.dto.voteBox.VoteBoxDto;
-//
-//import java.util.List;
-//
-//public interface VoteBoxService {
-//
-//  void saveVote(VoteBoxDto voteBoxDto);
-//
-//  List<VoteBox> getVoteBoxInfo(Long voteId);
-//}
+
+package com.project.voting.service.voteBox;
+
+import com.project.voting.domain.voteBox.VoteBox;
+import com.project.voting.dto.voteBox.VoteBoxDto;
+import java.util.List;
+
+public interface VoteBoxService {
+
+  List<VoteBox> getVoteBoxInfo(Long voteId);
+  void saveVote(VoteBoxDto voteBoxDto);
+  boolean isValid(VoteBoxDto voteBoxDto);
+
+}
