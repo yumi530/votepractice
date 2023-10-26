648af9b6c78275656d8b4de8b4376e5b72fc4baf
yumin lee <chenj7498@gmail.com>
2023-10-26 12:31:39 +0900

    선호도 투표 validation 수정

--- src/main/java/com/project/voting/service/voteBox/PreferenceVoteBoxService.java
+++ src/main/java/com/project/voting/service/voteBox/PreferenceVoteBoxService.java
@@ -3,8 +3,12 @@ package com.project.voting.service.voteBox;
 import com.project.voting.domain.vote.VoteType;
 import com.project.voting.domain.voteBox.VoteBox;
 import com.project.voting.dto.voteBox.VoteBoxDto;
+import com.project.voting.exception.vote_box.VoteBoxCustomException;
+import com.project.voting.exception.vote_box.VoteBoxErrorCode;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,18 +21,35 @@ public class PreferenceVoteBoxService extends VoteBoxService {
     List<Integer> ranks = voteBoxDto.getRanks();
 
     for (int i = 0; i < candidateIds.size(); i++) {
-      VoteBox voteBox = createVoteBox(voteBoxDto);
-      voteBox.setCandidateId(candidateIds.get(i));
-      voteBox.setChoices(0);
-      voteBox.setScores(0);
-      voteBox.setRanks(ranks != null && !ranks.isEmpty() ? ranks.get(i) : 0);
-      voteBoxes.add(voteBox);
-    }
-     voteBoxRepository.saveAll(voteBoxes);
+
+        VoteBox voteBox = createVoteBox(voteBoxDto);
+        voteBox.setCandidateId(candidateIds.get(i));
+        voteBox.setChoices(0);
+        voteBox.setScores(0);
+        voteBox.setRanks(isRankListValid(ranks,candidateIds.size()) ? ranks.get(i) : 0);
+        voteBoxes.add(voteBox);
+      }
+
+    voteBoxRepository.saveAll(voteBoxes);
   }
+
 
   @Override
   public VoteType getVoteType() {
     return VoteType.PREFERENCE;
   }
+
+  private boolean isRankListValid(List<Integer> ranks, int candidateCount) {
+    Set<Integer> usedRanks = new HashSet<>();
+    for (int rank : ranks) {
+      if (rank >= 1 && rank <= candidateCount && !usedRanks.contains(rank)) {
+        usedRanks.add(rank);
+      } else {
+        throw new VoteBoxCustomException(VoteBoxErrorCode.PREFER_NOT_VALID);
+      }
+    }
+    return usedRanks.size() == candidateCount;
+  }
+
+
 }
